8c4c204e5e7d1560187081c789e4304fd3591a34
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-06-04 19:12:46 -0500

    Terminacion de la guia 7.Dmv

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@
 //import Listfood from "./components/componente1/recetas";
 //import Button1 from "./components/componente1/button";
 //import Toolbar from "./components/componente1/Toolbar"
-import Signup from "./components/componente1/Signup";
+//import Signup from "./components/componente1/Signup";
+import Cards from "./components/componente1/cards";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Signup/>
+        <Cards/>
       </div>
     </>
   )
--- src/components/componente1/cards.jsx
+++ src/components/componente1/cards.jsx
@@ -4,14 +4,13 @@ import Card from 'react-bootstrap/Card';
 export default function Cards() {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src="\public\ChicaAprendiendoMagia.jpg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Aprendiendo</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Adquirir el conocimiento de algo por medio del estudio o de la experiencia
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary"><a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwir1aG26qr_AhVXs4QIHS7EAe4QFnoECA8QAw&url=https%3A%2F%2Fdle.rae.es%2Faprender%23%3A~%3Atext%3D1.%2Cestudio%2520o%2520de%2520la%2520experiencia.&usg=AOvVaw0f33pB2_Vm9mq5Ur4Zc6at">Ve a la pagina</a></Button>
       </Card.Body>
     </Card>
   );
